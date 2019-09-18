f56be7897bec42e643173fc640ce297e858e96b0
DieterDraelants <dieter.draelants@student.ucll.be>
2019-09-18 18:24:12 +0200

    de excecutable staat nu in Appeke

--- src/domain/Shop.java
+++ src/domain/Shop.java
@@ -1,8 +1,7 @@
 package domain;
 
+import javax.swing.*;
 import java.util.ArrayList;
-
-import javax.swing.JOptionPane;
 
 // tekst
 public class Shop {
@@ -30,24 +29,6 @@ public class Shop {
             price = days * 3;
         }
         return price;
-    }
-
-    public static void main(String[] args) {
-        Shop shop = new Shop();
-
-        String menu = "1. Add product\n2. Show product\n3. Show rental price\n\n0. Quit";
-        int choice = -1;
-        while (choice != 0) {
-            String choiceString = JOptionPane.showInputDialog(menu);
-            choice = Integer.parseInt(choiceString);
-            if (choice == 1) {
-                addProduct(shop);
-            } else if (choice == 2) {
-                showProduct(shop);
-            } else if (choice == 3){
-                showPrice(shop);
-            }
-        }
     }
 
     public static void addProduct(Shop shop) {
