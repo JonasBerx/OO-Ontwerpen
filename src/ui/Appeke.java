a8708fd6ec8c3594360fda1c338afd3efa87d038
DieterDraelants <dieter.draelants@student.ucll.be>
2019-09-19 23:15:14 +0200

    try catch toegevoegd

--- src/ui/Appeke.java
+++ src/ui/Appeke.java
@@ -8,20 +8,24 @@ import javax.swing.*;
 public class Appeke {
 
     public static void main(String[] args) throws DomainException {
-        Shop shop = new Shop();
+        try {
+            Shop shop = new Shop();
 
-        String menu = "1. Add product\n2. Show product\n3. Show rental price\n\n0. Quit";
-        int choice = -1;
-        while (choice != 0) {
-            String choiceString = JOptionPane.showInputDialog(menu);
-            choice = Integer.parseInt(choiceString);
-            if (choice == 1) {
-                shop.addProduct(shop);
-            } else if (choice == 2) {
-                shop.showProduct(shop);
-            } else if (choice == 3){
-                shop.showPrice(shop);
+            String menu = "1. Add product\n2. Show product\n3. Show rental price\n\n0. Quit";
+            int choice = -1;
+            while (choice != 0) {
+                String choiceString = JOptionPane.showInputDialog(menu);
+                choice = Integer.parseInt(choiceString);
+                if (choice == 1) {
+                    shop.addProduct(shop);
+                } else if (choice == 2) {
+                    shop.showProduct(shop);
+                } else if (choice == 3) {
+                    shop.showPrice(shop);
+                }
             }
+        } catch (DomainException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
