60ce51adf296c35aa3305e5730ec189d3073c507
Pieter Fiers <Ubipo.Skippy@gmail.com>
2019-09-20 12:10:02 +0200

    Added menu choice exception handling

--- src/ui/Appeke.java
+++ src/ui/Appeke.java
@@ -9,15 +9,20 @@ public class Appeke {
     static Shop shop = new Shop();
 
     public static void main(String[] args) throws DomainException {
-
         try {
-
-
             String menu = "1. Add product\n2. Show product\n3. Show rental price\n\n0. Quit";
             int choice = -1;
             while (choice != 0) {
                 String choiceString = JOptionPane.showInputDialog(menu);
-                choice = Integer.parseInt(choiceString);
+                if (choiceString == null || choiceString.isEmpty()) {
+                    continue;
+                }
+                try {
+                    choice = Integer.parseInt(choiceString);
+                } catch (NumberFormatException e) {
+                    JOptionPane.showMessageDialog(null, "Invalid menu choice: \"" + choiceString + "\", try again.");
+                    continue;
+                }
                 if (choice == 1) {
                     addProductUi();
                 } else if (choice == 2) {
